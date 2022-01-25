4ebf27492b7c3d9c0298b495926ccb9552e65f6e
Kannan MS <94454999+kannanshyam@users.noreply.github.com>
2022-01-25 10:29:15 +0530

    version 4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<h1><center>Flask Application Version 3</h1></center>"
+  return "<h1><center>Flask Application Version 4</h1></center>"
 
 
 app.run(host='0.0.0.0', port=5000)
